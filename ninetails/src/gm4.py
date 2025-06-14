a187bd57d033e336cb038c1e7d1baf83df1e2575
Antoinehoff <antoine-hoffmann@hotmail.com>
2025-06-14 17:15:39 -0400

    put back collision

--- ninetails/src/gm4.py
+++ ninetails/src/gm4.py
@@ -23,10 +23,10 @@ def GM4(model, t, y):
     # Equation (A1): density (p=0, j=0)
     model.dydt[0]  = +sqrt_tau * (model.Cpar(n10) - model.CparB(n10))
     model.dydt[0] += -tau * model.Cperp(2*n00 + sqrt2*n20 - n01) 
-    model.dydt[0] += +model.K1 * model.p.RT * model.iky * phi 
+    model.dydt[0] += +model.K0 * model.p.RT * model.iky * phi 
     model.dydt[0] += -model.K0 * model.p.RN * model.iky * phi
+    model.dydt[0] += -2./3. * model.p.nu * model.lperp * (5*n00 + sqrt2 * n20)
     model.dydt[0] += -model.p.muHD * model.kperp2 * y[0]
-    # self.dydt[0] += -2/3 * self.p.nu * self.lperp * (5*n00 + sqrt2 * n20)
     
     # Equation (A2): parallel velocity (p=1, j=0)
     model.dydt[1]  = +sqrt_tau * model.Cpar(n00 + sqrt2*n20)
@@ -40,8 +40,8 @@ def GM4(model, t, y):
     model.dydt[2] += -sqrt_tau * model.CparB(sqrt3*n30 + sqrt2*n11)
     model.dydt[2] += +tau * model.Cperp(6*n20 + sqrt2*n00 + sqrt12*n40 - n21)
     model.dydt[2] += -0.5 * sqrt2 * model.p.RT * model.iky * model.K0 * phi
+    model.dydt[2] += -2./3. * model.p.nu * (2*sqrt2*n00 + 2*n20 + sqrt2*n01)
     model.dydt[2] += -model.p.muHD * model.kperp2 * y[2]
-    # self.dydt[2] += -2/3 * self.p.nu * (2*sqrt2*n00 + 2*n20 + sqrt2*n01)
 
     # Equation (A4): perpendicular temperature (p=0, j=1)
     model.dydt[3]  = +sqrt_tau * model.Cpar(n11)
@@ -49,9 +49,8 @@ def GM4(model, t, y):
     model.dydt[3] += +tau * model.Cperp(4*n01 - n00 + sqrt2*n21 - 2*n02)
     model.dydt[3] += -model.p.RT * model.iky * (2*model.K1 - model.K0 - 2*model.K2) * phi
     model.dydt[3] += -model.p.RN * model.iky * model.K1 * phi
-    model.dydt[3] += -2/3 * model.p.nu * (2*n00 + sqrt2*n20 + n01)
+    model.dydt[3] += -2./3. * model.p.nu * (2*n00 + sqrt2*n20 + n01)
     model.dydt[3] += -model.p.muHD * model.kperp2 * y[3]
-    # self.dydt[3] += -2/3 * self.p.nu * (2*n00 + sqrt2*n20 + n01)
 
     # Compute the nonlinear terms using Poisson brackets if enabled
     if model.nonlinear:
@@ -61,7 +60,7 @@ def GM4(model, t, y):
 
         # Equation (A1): density
         model.dydt[0] -= model.pb.compute(K0phi, N00)
-        #self.dydt[0] -= self.pb.compute(K1phi, N01)
+        #model.dydt[0] -= model.pb.compute(K1phi, N01)
         
         # Equation (A2): parallel velocity
         model.dydt[1] -= model.pb.compute(K0phi, n10)
